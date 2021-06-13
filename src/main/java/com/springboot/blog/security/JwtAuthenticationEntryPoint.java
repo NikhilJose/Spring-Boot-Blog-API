0e9c476db06ab652348fd64a355d052fccd1b1af
NikhilJose <nikhiljose002@gmail.com>
2021-06-13 14:23:44 +0530

    Added JWT

---
+++ src/main/java/com/springboot/blog/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.springboot.blog.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // this method is called when an exception is thrown due to an
+    // unauthenticated user trying to access a resource that requires authentication
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+
+    }
+}
