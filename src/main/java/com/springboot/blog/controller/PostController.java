b819953b019e518848aacd8713ad0c48deb07b91
NikhilJose <nikhiljose002@gmail.com>
2021-06-13 17:23:14 +0530

    Versioning Blog App Rest API

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,7 +1,6 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.PostDto;
-import com.springboot.blog.payload.PostDtoV2;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
@@ -11,8 +10,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping
@@ -40,25 +37,9 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
-    @GetMapping(value = "/api/posts/{id}", produces = "application/vnd.nikhil.v1+json")
+    @GetMapping(value = "/api/v1/posts/{id}")
     public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id) {
         return ResponseEntity.ok(postService.getPost(id));
-    }
-
-    @GetMapping(value = "/api/posts/{id}", produces = "application/vnd.nikhil.v2+json")
-    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id) {
-        PostDto postDto = postService.getPost(id);
-        PostDtoV2 postDtoV2 = new PostDtoV2();
-        postDtoV2.setId(postDto.getId());
-        postDtoV2.setTitle(postDto.getTitle());
-        postDtoV2.setDescription(postDto.getDescription());
-        postDtoV2.setContent(postDto.getContent());
-        List<String> tags = new ArrayList<>();
-        tags.add("Java");
-        tags.add("Spring Boot");
-        tags.add("AWS");
-        postDtoV2.setTags(tags);
-        return ResponseEntity.ok(postDtoV2);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
